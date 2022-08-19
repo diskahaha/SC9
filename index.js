6e80e60338909abf03e4498a59161f953daaacd2
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-08-19 17:00:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -144,13 +144,6 @@ async function startxavior() {
             for (let num of participants) {
                 // Get Profile Picture User
                 try {
-                    ppuser = await xavior.profilePictureUrl(num, 'image')
-                } catch {
-                    ppuser = 'https://tinyurl.com/yx93l6da'
-                }
-
-                // Get Profile Picture Group
-                try {
                     ppgroup = await xavior.profilePictureUrl(anu.id, 'image')
                 } catch {
                     ppgroup = 'https://tinyurl.com/yx93l6da'
